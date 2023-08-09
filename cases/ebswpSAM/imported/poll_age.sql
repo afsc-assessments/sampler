4e5211211a9f718ecfa5e52646e91fe01e7b3317
Jim.Ianelli <Jim.Ianelli@nmfs.local>
2023-08-08 18:00:27 -0700

    added some refinements to sql

--- cases/ebswpSAM/imported/poll_age.sql
+++ cases/ebswpSAM/imported/poll_age.sql
@@ -1,9 +1,11 @@
 set colsep ",";
 set pagesize 0;
 set trimspool on;
+set TERMOUT off;
+set linesize 1000;
 set headsep off;
-spool 'c:\users\jim.ianelli\testage.csv';
+spool 'poll_age22.csv';
 select 
 YEAR, HAUL_OFFLOAD, HAUL_OFFLOAD_DATE,NMFS_AREA,GEAR,FMP_SUBAREA,FMP_GEAR,LENGTH,weight,age,SEX
-from norpac.debriefed_age_flat_mv where species=201;
+from norpac.debriefed_age_flat_mv where species=201 and fmp_subarea='BS' and year=2022;
 spool off;
--- cases/ebswpSAM/imported/poll_len.sql
+++ cases/ebswpSAM/imported/poll_len.sql
@@ -1,8 +1,12 @@
 set colsep ,;
 set pagesize 0;
+set linesize 1000;
 set trimspool on;
+set TERMOUT off;
 set headsep off;
-spool 'c:\users\jim.ianelli\poll_len.csv';
+spool 'poll_len.csv';
 select 
 YEAR, HAUL_OFFLOAD, HAUL_OFFLOAD_DATE,NMFS_AREA,GEAR,FMP_SUBAREA,FMP_GEAR,LENGTH,FREQUENCY,SEX
-from norpac.debriefed_length_mv where species=201;
+from norpac.debriefed_length_mv where species=201 and 
+FMP_SUBAREA='BS' and YEAR=2022;
+spool off
